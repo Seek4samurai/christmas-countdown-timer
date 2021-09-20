5b82c04eec47f996d35c55ea6161b8b0c08b7d3d
Gourav Singh Rawat <69115613+Seek4samurai@users.noreply.github.com>
2021-09-20 09:52:18 +0530

    added js

---
+++ script.js
@@ -0,0 +1,33 @@
+//Constants--------------------------------------------
+const daysElement = document.getElementById("days");
+const hoursElement = document.getElementById("hours");
+const minsElement = document.getElementById("mins");
+const secondsElement = document.getElementById("seconds");
+
+const newYeareve = "1 Jan 2021";
+
+//Functions here---------------------------------------
+function countdown(){
+    const newYearDate = new Date(newYeareve);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearDate - currentDate) / 1000;
+    
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;  
+
+    daysElement.innerHTML = days;
+    hoursElement.innerHTML = formattime(hours);
+    minsElement.innerHTML = formattime(mins);
+    secondsElement.innerHTML = formattime(seconds);
+}
+
+function formattime(time){
+    return time < 10 ? `0${time}` : time;
+}
+
+countdown();
+
+setInterval(countdown, 1000);
